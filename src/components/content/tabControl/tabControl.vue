bae6c8be8ecef26aaf7395a8a2b176180f0c275c
TaiYiZhen <1525067375@qq.com>
2019-12-25 16:26:33 +0800

    home update

---
+++ src/components/content/tabControl/tabControl.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" class="tab-control-item"
+    :class="{active:index==currentIndex}"
+    @click="itemclick(index)" :key="item.name">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "tabControl",
+    props: {
+      titles: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0
+      }
+    },
+    methods:{
+      itemclick(index){
+        this.currentIndex=index;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+    background-color: white;
+  }
+
+  .tab-control-item {
+    flex: 1;
+  }
+
+  .tab-control-item span{
+    padding: 5px;
+  }
+
+  .active {
+    color: hotpink;
+
+  }
+
+  .active span{
+    border-bottom: 2px solid hotpink;
+
+  }
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control :titles="['流行','新款','精选']"/>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+    <h1>--------------</h1>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar';
+  import tabControl from 'components/content/tabControl/tabControl'
+
+  import HomeSwiper from './childComps/HomeSwiper';
+  import RecommendView from './childComps/RecommendView';
+  import FeatureView from './childComps/FeatureView.vue'
+
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      tabControl
+    },
+    data(){
+      return{
+        banners:[],
+        recommends:[],
+        goods:{
+          'pop':{page:0,list:[]},
+          'news':{page:0,list:[]},
+          'sell':{page:0,list:[]},
+        }
+      }
+    },
+    created(){
+      //1.请求多个数据
+     getHomeMultidata().then(res => {
+       // this.result = res;
+       this.banners = res.data.banner.list;
+       this.recommends = res.data.recommend.list;
+     })
+    }
+  }
+</script>
+
+<style scoped>
+  #home{
+    padding-top: 44px;
+  }
+  .home-nav{
+    background-color: var(--color-tint);
+    color: white;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top:0;
+    z-index: 9;
+  }
+
+  .tab-control{
+    position: sticky;
+    top: 44px;
+  }
+</style>
